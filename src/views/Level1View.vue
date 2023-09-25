f6dab1ca4cef68d88c762c06ab64f74f3a5df3b5
Jeven Dale Marfil <jeven.marfil@rev-kitten.com>
2023-09-25 15:31:42 +0800

    first commit

---
+++ src/views/Level1View.vue
@@ -0,0 +1,167 @@
+<template>
+  <div style="display: flex;">
+    <div class="title">
+      <h1>Drag the Letter Game</h1>
+      <div class="letter-container">
+        <div
+          v-for="(letter, index) in unshuffledLetters"
+          :key="index"
+          class="letter"
+          :style="{ backgroundColor: letter.color, color: 'white' }"
+          @mousedown="startDrag(letter.text)"
+          draggable="true"
+        >
+          {{ letter.text }}
+        </div>
+      </div>
+    </div>
+    <div class="grid">
+      <div v-for="(row, rowIndex) in grid" :key="rowIndex" class="row">
+        <div
+          v-for="(cell, colIndex) in row"
+          :key="colIndex"
+          class="cell"
+          @dragover.prevent
+          @drop="dropLetter(cell.letter, rowIndex, colIndex)"
+        >
+          <div class="drop-zone">
+            <div class="letter" :style="{ backgroundColor: cell.color }" v-if="cell.isPlaced == 1">{{ cell.letter }}</div>
+            <div v-else>{{ cell.letter }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const unshuffledLetters = ref<any>([])
+const letters = ref<any>([])
+const selectedLetter = ref({text: null, color: ''})
+const droppedLetter = ref({text: null, color: ''})
+const grid = ref<any>([])
+const colors = ['green', 'purple']; // Define the two alternating colors
+let colorIndex = 0; // Initialize the color index
+
+//methods
+onMounted(() => {
+  for (let letterCode = 65; letterCode <= 90; letterCode++) {
+    const color = colors[colorIndex]; // Get the current color
+    letters.value.push({
+        text: String.fromCharCode(letterCode),
+        color: color
+    })
+    unshuffledLetters.value.push({
+        text: String.fromCharCode(letterCode),
+        color: color
+    })
+    colorIndex = 1 - colorIndex; // Toggle the color index
+  }
+
+  // Add numbers '1' to '10'
+  for (let number = 1; number <= 10; number++) {
+    const color = colors[colorIndex]; // Get the current color
+    letters.value.push({
+        text: String(number),
+        color: color
+    })
+    unshuffledLetters.value.push({
+        text: String(number),
+        color: color
+    })
+    colorIndex = 1 - colorIndex; 
+
+  }
+
+  generateGrid()
+})
+
+const startDrag = (letter: any) => {
+  selectedLetter.value.text = letter
+}
+
+const endDrag = () => {
+  selectedLetter.value = null
+}
+
+const dropLetter = (letter: any, rowIndex: number, colIndex: number) => {
+  if (selectedLetter.value.text !== null && selectedLetter.value.text === letter) {
+    console.log('DROP LOCATION', letter)
+    grid.value[rowIndex][colIndex].isPlaced = 1
+    // letters.value.splice(index, 0, selectedLetter.value);
+    droppedLetter.value = selectedLetter.value
+    selectedLetter.value.text = null
+  }
+}
+
+const generateGrid = () => {
+  // Shuffle the letters array randomly
+  shuffleArray(letters.value)
+
+  // Initialize the 6x6 grid
+  for (let i = 0; i < 6; i++) {
+    grid.value.push([])
+    for (let j = 0; j < 6; j++) {
+      grid.value[i].push({
+        letter: letters.value[i * 6 + j].text,
+        color: letters.value[i * 6 + j].color,
+        isPlaced: 0
+      })
+    }
+  }
+}
+
+const shuffleArray = (array: any) => {
+  // Fisher-Yates shuffle algorithm
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+.letter-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.letter {
+  width: 60px;
+  height: 60px;
+  background-color: #3498db;
+  color: #fff;
+  text-align: center;
+  line-height: 60px;
+  margin: 5px;
+  cursor: pointer;
+  border-radius: 50%; /* Add this line */
+}
+
+.grid {
+  display: grid;
+  grid-template-rows: repeat(6, 0fr);
+  grid-gap: 0px; /* Adjust the gap between cells as needed */
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px; /* Adjust font size as needed */
+}
+
+.cell {
+  width: 100px; /* Adjust the cell width as needed */
+  height: 100px; /* Adjust the cell height as needed */
+  border: 1px solid #ccc; /* Add borders to cells if desired */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
